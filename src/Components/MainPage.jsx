90d0bf1bd19d9b6aeb32eb771fbde59d83236d15
Kris <krismahadhika@gmail.com>
2022-05-07 15:35:53 +0800

    adding pdf

---
+++ src/Components/MainPage.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+function MainPage() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div>
+      <Document
+
+                file="python.pdf"
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+    </div>
+  );
+}
+
+
+export default MainPage;
